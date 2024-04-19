3ead100f9e2a479a58daaff83e8f728263d97370
funkim <mcking172@skiff.com>
2024-04-19 19:42:30 -0400

    added some minor ui

--- src/createWeather.js
+++ src/createWeather.js
@@ -1,47 +1,57 @@
 import { getWeather } from "./getWeather";
 
+const aContainer = document.querySelector('.container');
+
 export function createWeatherOnPage(baltimoreWeatherData) {
-    getLocation(baltimoreWeatherData)
-    getTime(baltimoreWeatherData)
-    tempature(baltimoreWeatherData)
+    getLocation(baltimoreWeatherData);
+    getTime(baltimoreWeatherData);
+    tempature(baltimoreWeatherData);
     feelsLike(baltimoreWeatherData);
-    humidity(baltimoreWeatherData)
+    humidity(baltimoreWeatherData);
 }
 
 function getLocation(baltimoreWeatherData) {
     const country = document.createElement("h1");
     const area = document.createElement("h2");
     const state = document.createElement("h2");
+
+    country.classList.add('country');
+    area.classList.add('area');
+    state.classList.add('state');
+
     country.textContent = `${baltimoreWeatherData.location.country}`;
     area.textContent = `${baltimoreWeatherData.location.name}`;
     state.textContent = `${baltimoreWeatherData.location.region}`;
-    document.body.appendChild(country);
-    document.body.appendChild(state);
-    document.body.appendChild(area);
-    console.log(country.textContent);
+
+    aContainer.appendChild(country);
+    aContainer.appendChild(state);
+    aContainer.appendChild(area);
 }
 
 function getTime(baltimoreWeatherData) {
-    const localTime = document.createElement("h2")
-    localTime.textContent = `${baltimoreWeatherData.location.localtime}`
-    document.body.appendChild(localTime);
+    const localTime = document.createElement("h2");
+    localTime.classList.add('localTime');
+    localTime.textContent = `${baltimoreWeatherData.location.localtime}`;
+    aContainer.appendChild(localTime);
 }
 
 function tempature(baltimoreWeatherData) {
     const temp = document.createElement("h2");
+    temp.classList.add('temp');
     temp.textContent = `${baltimoreWeatherData.current.temp_f}°F`;
-    document.body.appendChild(temp);
-    console.log(temp.textContent);
+    aContainer.appendChild(temp);
 }
+
 function humidity(baltimoreWeatherData) {
     const humidity = document.createElement("h2");
+    humidity.classList.add('humidity');
     humidity.textContent = `Humidity Level: ${baltimoreWeatherData.current.humidity}`;
-    document.body.appendChild(humidity);
-    console.log(humidity.textContent);
+    aContainer.appendChild(humidity);
 }
+
 function feelsLike(baltimoreWeatherData) {
     const feelsTemp = document.createElement("h2");
+    feelsTemp.classList.add('feelsTemp');
     feelsTemp.textContent = `Feels like: ${baltimoreWeatherData.current.feelslike_f}°F`;
-    document.body.appendChild(feelsTemp);
-    console.log(feelsTemp.textContent);
+    aContainer.appendChild(feelsTemp);
 }
